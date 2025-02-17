11014e801d2ef73d0e7e616b4e5bd94261512809
Cha@DESKTOP-AS4EO9U <eve0110010010@gmail.com>
2025-02-17 15:30:42 +0800

    Update search algorithm
    Compares length of songs instead of only matching the first song result

--- main.py
+++ main.py
@@ -10,48 +10,63 @@ def extract_playlist_id(url):
         url = url.lstrip("https://open.spotify.com/playlist/").split("?si=")[0]
     return url
     
-def get_playlist_titles(playlist_id):
-    """Returns list of song titles with artist names in specified playlist."""
+def get_playlist_info(playlist_id):
+    """Extracts data from Spotify and return them in format
+       `[{"title": title, "artist": artist, "length": length}]`."""
 
     items = next(Public.playlist_info(playlist_id))["items"]
 
-    songs = []
+    result = []
     
     for item in items:
+        song = {}
         item = item["itemV2"]["data"]
-        title = item["name"]
-        artist = item["artists"]["items"][0]["profile"]["name"]
-        songs.append(f"{title} - {artist}")
+        song["title"] = item["name"]
+        song["artist"] = item["artists"]["items"][0]["profile"]["name"]
+        song["length"] = int(item["trackDuration"]["totalMilliseconds"])
+        result.append(song)
 
-    return songs
+    return result
 
-def get_song_url(search_text):
-    """Simulates searching from the YTMusic web and returns the url
-    of first song result."""
+def convert_to_milliseconds(text):
+    """Converts `"%M:%S"` timestamp from YTMusic to milliseconds."""
+    minutes, seconds = text.split(":")
+    return (int(minutes) * 60 + int(seconds)) * 1000
+    
+def get_song_url(song_info):
+    """Simulates searching from the YTMusic web and returns url to closest match."""
 
     client = InnerTube("WEB_REMIX", "1.20250203.01.00")
-    data = client.search(search_text)
-    
-    video_id = data["contents"]["tabbedSearchResultsRenderer"]["tabs"][0][
-               "tabRenderer"]["content"]["sectionListRenderer"][
-               "contents"][1]["musicShelfRenderer"]["contents"][0][
-               "musicResponsiveListItemRenderer"]["overlay"][
-               "musicItemThumbnailOverlayRenderer"]["content"][
-               "musicPlayButtonRenderer"]["playNavigationEndpoint"][
-               "watchEndpoint"]["videoId"]
+    data = client.search(f"{song_info['title']} {song_info['artist']}")
+
+    top_result_length = data["contents"]["tabbedSearchResultsRenderer"]["tabs"][0]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][0]["musicCardShelfRenderer"]["subtitle"]["runs"][-1]["text"]
+    first_song_length = data["contents"]["tabbedSearchResultsRenderer"]["tabs"][0]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][1]["musicShelfRenderer"]["contents"][0]["musicResponsiveListItemRenderer"]["flexColumns"][1]["musicResponsiveListItemFlexColumnRenderer"]["text"]["runs"][-1]["text"]
+
+    top_result_diff = abs(convert_to_milliseconds(top_result_length) - song_info["length"])
+    first_song_diff = abs(convert_to_milliseconds(first_song_length) - song_info["length"])
+
+    if top_result_diff < first_song_diff:
+        # get top result url
+        video_id = data["contents"]["tabbedSearchResultsRenderer"]["tabs"][0]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][0]["musicCardShelfRenderer"]["title"]["runs"][0]["navigationEndpoint"]["watchEndpoint"]["videoId"]
+        video_title = data["contents"]["tabbedSearchResultsRenderer"]["tabs"][0]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][0]["musicCardShelfRenderer"]["title"]["runs"][0]["text"]
+    else:
+        # get first song result url
+        video_id = data["contents"]["tabbedSearchResultsRenderer"]["tabs"][0]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][1]["musicShelfRenderer"]["contents"][0]["musicResponsiveListItemRenderer"]["overlay"]["musicItemThumbnailOverlayRenderer"]["content"]["musicPlayButtonRenderer"]["playNavigationEndpoint"]["watchEndpoint"]["videoId"]
+        video_title = data["contents"]["tabbedSearchResultsRenderer"]["tabs"][0]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][1]["musicShelfRenderer"]["contents"][0]["musicResponsiveListItemRenderer"]["flexColumns"][0]["musicResponsiveListItemFlexColumnRenderer"]["text"]["runs"][0]["text"]
 
     url = "https://music.youtube.com/watch?v=" + video_id
 
-    return url
+    return url, video_title
 
-def get_song_urls(titles):
-    """Repeatedly calls get_song_url on given titles. Return list of results."""
+def get_song_urls(playlist_info):
+    """Repeatedly calls get_song_url on given playlist info. Returns list of results."""
     urls = []
-    for title in titles:
-        print(f"Getting url for {title}")
-        result = get_song_url(title)
-        urls.append(result)
-        print(f"{title}: {result}")
+    
+    for song_info in playlist_info:
+        print(f"Getting url for {song_info['title']}")
+        result = get_song_url(song_info)
+        urls.append(result[0])
+        print(f"{result[1]} ({result[0]})")
         sleep(uniform(1, 3))
 
     return urls
@@ -74,13 +89,14 @@ def download_from_urls(urls):
                              'only_multi_video': True,
                              'when': 'playlist'}],
          'retries': 10}
-    
+
+    # downloads stream with highest bitrate, then save them in m4a format
     with YoutubeDL(options) as ydl:
         ydl.download(urls)
     
 def main(playlist_id):
-    titles = get_playlist_titles(playlist_id)
-    download_urls = get_song_urls(titles)
+    playlist_info = get_playlist_info(playlist_id)
+    download_urls = get_song_urls(playlist_info)
     download_from_urls(download_urls)
     
 if __name__ == "__main__":
